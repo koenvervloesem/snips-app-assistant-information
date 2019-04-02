d262b8d934187874cde8eec5798f1a276aa9008a
philipp2310 <41761223+philipp2310@users.noreply.github.com>
2019-04-02 23:56:40 +0200

    Create de.py

---
+++ translations/de.py
@@ -0,0 +1,23 @@
+"""
+This module contains the result sentences and intents for the German version
+of the Assistant information app.
+"""
+
+# Result sentences
+RESULT_ASSISTANT_APPS = "Ich habe {} apps: {}."
+RESULT_ASSISTANT_ID = "Meine ID ist {}."
+RESULT_ASSISTANT_INTENTS = "Ich kenne {} Befehle."
+RESULT_ASSISTANT_NAME = "Mein Name ist {}."
+RESULT_ASSISTANT_PLATFORM = "Ich laufe auf der {} platform."
+RESULT_IP_ADDRESS = "Meine IP Adresse ist {}."
+RESULT_SNIPS_VERSION = "Ich laufe mit Snips version {}."
+AND = ", und "
+
+# Intents
+INTENT_ASSISTANT_APPS = 'Philipp:AssistantApps'
+INTENT_ASSISTANT_ID = 'Philipp:AssistantID'
+INTENT_ASSISTANT_INTENTS = 'Philipp:AssistantIntents'
+INTENT_ASSISTANT_NAME = 'Philipp:AssistantName'
+INTENT_ASSISTANT_PLATFORM = 'Philipp:AssistantPlatform'
+INTENT_IP_ADDRESS = 'Philipp:IPAddress'
+INTENT_SNIPS_VERSION = 'Philipp:SnipsVersion'
