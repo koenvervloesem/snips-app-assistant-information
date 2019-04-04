418feec30a1d74e7fc88bc4d470c1b0a584db3b9
philipp2310 <41761223+philipp2310@users.noreply.github.com>
2019-04-04 22:57:25 +0200

    Update de.py
    added hostname
    added raspi for TTS

--- translations/de.py
+++ translations/de.py
@@ -9,9 +9,12 @@ RESULT_ASSISTANT_ID = "Meine ID ist {}."
 RESULT_ASSISTANT_INTENTS = "Ich kenne {} Befehle."
 RESULT_ASSISTANT_NAME = "Mein Name ist {}."
 RESULT_ASSISTANT_PLATFORM = "Ich laufe auf der {} platform."
+RESULT_HOSTNAME = "Mein Host-Name ist {}."
 RESULT_IP_ADDRESS = "Meine IP Adresse ist {}."
 RESULT_SNIPS_VERSION = "Ich laufe mit Snips version {}."
 AND = ", und "
+
+REPLACE_TTS_RASPI = "Raspberrie Pei"
 
 # Intents
 INTENT_ASSISTANT_APPS = 'Philipp:AssistantApps'
@@ -19,5 +22,6 @@ INTENT_ASSISTANT_ID = 'Philipp:AssistantID'
 INTENT_ASSISTANT_INTENTS = 'Philipp:AssistantIntents'
 INTENT_ASSISTANT_NAME = 'Philipp:AssistantName'
 INTENT_ASSISTANT_PLATFORM = 'Philipp:AssistantPlatform'
+INTENT_HOSTNAME = 'Philipp:Hostname'
 INTENT_IP_ADDRESS = 'Philipp:IPAddress'
 INTENT_SNIPS_VERSION = 'Philipp:SnipsVersion'
